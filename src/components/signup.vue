001c167b948da46aba56d061d786526e9b72dbbb
Senan747 <111648854+Senan747@users.noreply.github.com>
2023-03-02 21:17:22 +0400

    third commit

--- src/components/signup.vue
+++ src/components/signup.vue
@@ -1,10 +1,11 @@
 <template>
-  <form action="#">
+  <form @submit="handleSubmit()">
     <label>Email:</label>
     <input type="email" required v-model="email">
 
     <label>Password:</label>
     <input type="password" required v-model="password">
+    <div v-if="passwordError" class="error">!{{ passwordError }}</div>
  
     <label>Role</label>
     <select v-model="role">
@@ -15,12 +16,16 @@
     <label>Skills</label>
     <input type="text" v-model="tempSkills" @keyup="addSkill">
     <div v-for="skill in skills" :key="skill" class="pill">
-      {{ skill }}
+      <span @click="deleteSkill(skill)">{{ skill }}</span> 
     </div>
 
     <div class="terms">
       <input type="checkbox" v-model="terms" required>
       <label>Accept terms and conditions</label>
+    </div>
+
+    <div class="submit">
+      <button>Create account</button>
     </div>
 </form>
 
@@ -29,7 +34,7 @@
   <p>Role: {{ role }}</p>
   <p>Terms: {{ terms }}</p>
   <p>Names: {{ names }}</p>
-  <p>Skills: {{ skills }}</p>
+  <p>Skills: {{ skills }}</p> 
 </template>
 
 <script>
@@ -37,13 +42,14 @@
 export default {
   data(){
     return{
-      email: 'mario',
+      email: '',
       password: '',
-      role: 'f-developer',
+      role: '',
       terms: false,
       names: [],
       tempSkills: '',
-      skills: []
+      skills: [],
+      passwordError: ''
     }
   },
   methods: {
@@ -54,6 +60,14 @@ export default {
       }
       this.tempSkills = ''
     }
+    },
+    deleteSkill(skill){
+      this.skills = this.skills.filter((item) => {
+        return skill !== item 
+      })
+    },
+    handleSubmit(){
+      this.passwordError = this.password.length > 6 ? '' : 'Password must be at least 6 chars long'
     }
   }
 }
@@ -72,7 +86,7 @@ export default {
     color: grey;
     display: inline-block;
     margin:25px 0 15px;
-    font-size: 0.6em;
+    font-size: 0.8em;
     text-transform: uppercase;
     letter-spacing: 1px;
     font-weight: bold;
@@ -92,5 +106,34 @@ export default {
     margin: 0 10px 0 0;
     position: relative;
     top: 2px;
+  }
+  .pill{
+    display: inline-block;
+    margin: 20px 10px 0 0;
+    padding: 6px 12px;
+    background-color: #eee;
+    border-radius: 20px;
+    font-size: 12px;
+    letter-spacing: 1px;
+    font-weight: bold;
+    color: #777;
+    cursor: pointer;
+  }
+  button{
+    border:none;
+    background-color: #49959e;
+    color:white;
+    padding: 10px 20px;
+    font-size: 16px;
+    border-radius: 7px;
+  }
+  .submit{
+    text-align: center;
+  }
+  .error{
+    color:red;
+    font-size:16px;
+    margin-top: 5px;
+    font-weight: 600; 
   }
 </style>
